801c5dc7b9483f21e69901401dee0cabdcfa6b50
djksdjkjkdjksdjk <gimpo.s230096@ggh.goe.go.kr>
2024-10-30 10:54:56 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,11 +12,11 @@ st.write('음수?? 어렵지않아!')
 #col1, col2 = st.columns(2)       
 # 왼쪽 문단
 with col1:
- st.subheader('**개념정리')
- st.write('- 양수: 0보다 큰 수')
- st.write('- 음수: 0보다 작은 수')
- st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
- st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
+      st.subheader('**개념정리')
+      st.write('- 양수: 0보다 큰 수')
+      st.write('- 음수: 0보다 작은 수')
+      st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
+      st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
 
 with col2:
  st.image('수학이미지.PNG')  # 이미지 파일 불러오기
@@ -27,8 +27,8 @@ a = st.number_input('수를 입력하세요', value=0)
 
 if st.button('양수일까 음수일까?')
        if a > 0:
-           st.write('양수 ')
+               st.write('양수 ')
        elif a < 0:
-           st.write('음수')
+               st.write('음수')
        else:
-           st.write('0')
+               st.write('0')
